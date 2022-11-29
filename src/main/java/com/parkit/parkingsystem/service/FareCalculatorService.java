e4c458644e45b8b0b4346316f4bb6d27bb05439c
ElHadji Omar DIONE <omardione1991@gmail.com>
2022-11-29 18:24:45 -0500

    refactoring

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,6 +1,5 @@
 package com.parkit.parkingsystem.service;
 
-import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
 import java.time.Duration;
@@ -8,7 +7,12 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
+import static com.parkit.parkingsystem.constants.Fare.BIKE_RATE_PER_HOUR;
+import static com.parkit.parkingsystem.constants.Fare.CAR_RATE_PER_HOUR;
+
 public class FareCalculatorService {
+
+    public static final int MINUTES_PARKING = 45;
 
     public void calculateFare(Ticket ticket) {
         if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
@@ -16,15 +20,15 @@ public class FareCalculatorService {
         }
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        long duration = calculateDuration(ticket.getInTime(), ticket.getOutTime());
+        Duration duration = calculateDuration(ticket.getInTime(), ticket.getOutTime());
 
         switch (ticket.getParkingSpot().getParkingType()) {
             case CAR: {
-                ticket.setPrice(duration == 45 ? Fare.CAR_RATE_PER_HOUR * 0.75 : duration / 60.0 * Fare.CAR_RATE_PER_HOUR);
+                ticket.setPrice(computePrice(duration, CAR_RATE_PER_HOUR));
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration == 45 ? Fare.BIKE_RATE_PER_HOUR * 0.75 : duration / 60.0 * Fare.BIKE_RATE_PER_HOUR);
+                ticket.setPrice(computePrice(duration, BIKE_RATE_PER_HOUR));
                 break;
             }
             default:
@@ -32,9 +36,21 @@ public class FareCalculatorService {
         }
     }
 
-    private long calculateDuration(Date in, Date out) {
+    private double computePrice(Duration duration, double ratePerHour) {
+        if (is45MinutesParking(duration)) {
+            return ratePerHour * 0.75;
+        } else {
+            return duration.toHours() * ratePerHour;
+        }
+    }
+
+    private boolean is45MinutesParking(Duration duration) {
+        return duration.toMinutes() == MINUTES_PARKING;
+    }
+
+    private Duration calculateDuration(Date in, Date out) {
         LocalDateTime start = in.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
         LocalDateTime outDate = out.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-        return Duration.between(start, outDate).toMinutes();
+        return Duration.between(start, outDate);
     }
 }
